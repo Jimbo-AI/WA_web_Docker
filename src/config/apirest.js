a6b0c56cfe7dfbcbe8e03c1ff0611160e7c39231
Daniboy2802 <dv.santibanez@gmail.com>
2024-03-03 19:19:37 -0600

    cors enabled for any route --change

--- src/config/apirest.js
+++ src/config/apirest.js
@@ -1,10 +1,12 @@
 const express = require('express')
+var cors = require('cors')
 
 const { envioController } = require('../controller/envio.controller')
 const { newSessionController } = require('../controller/new_session.controller')
 const { getSessionState } = require('../controller/health_check_controller')
 const apirest = express()
 apirest.use(express.json())
+apirest.use(cors())
 
 apirest.post('/envio', envioController)
 apirest.get('/session', newSessionController)
